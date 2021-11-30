3bc0cab964c861956543218a7a546247390c2f12
AlCh440 <73243813+AlCh440@users.noreply.github.com>
2021-11-30 12:52:43 +0100

    fixed a lot of things

--- Game/Source/Defs.h
+++ Game/Source/Defs.h
@@ -64,6 +64,6 @@ inline const char* const PATH(const char* folder, const char* file)
 // Performance macros
 #define PERF_START(timer) timer.Start()
 #define PERF_PEEK(timer) LOG("%s took %f ms", __FUNCTION__, timer.ReadMs())
-#define FPS 45
+#define FPS 60
 
 #endif	// __DEFS_H__
